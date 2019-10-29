27f38c45e9d526420e81b31e24201eeaec25f96f
PravarChaurasia <praverchaurasia@gmail.com>
2019-10-29 18:33:04 +0530

    Create tsh.java

---
+++ tsh.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Tshr
+{
+  public static void tsh(int pull)
+  {
+    if(pull!=0)
+    System.out.println("U will get one tsh");
+  }
+  public static void main(Strings args[])
+  {
+    int pull=1;
+    tsh(pull);
+  }
+}
